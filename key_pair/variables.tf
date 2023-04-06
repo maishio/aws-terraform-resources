1548fabd3891d5ff4a21b85de2dd3ae51dcb0956
maishio <masanori.ishio119@gmail.com>
2023-04-06 13:39:03 +0900

    feat: add key_pair resource

---
+++ key_pair/variables.tf
@@ -0,0 +1,18 @@
+# Amazo EC2 Key Pair
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/key_pair
+
+variable "key_name" {
+  description = "The name for the key pair."
+  type        = string
+}
+
+variable "public_key" {
+  description = "The public key material."
+  type        = string
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resource."
+  type        = map(string)
+  default     = {}
+}
