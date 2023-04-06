0e9f48bf28d16ece0973cb2bcd0044be5cf01804
maishio <masanori.ishio119@gmail.com>
2023-04-06 10:30:53 +0900

    refactor: modified route_table resource

--- route_table/outputs.tf
+++ route_table/outputs.tf
@@ -1,7 +1,5 @@
-# --------------------------------------------------------------------------------
-# Amazon VPC Route Table output
-# @see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
-# --------------------------------------------------------------------------------
+# Amazon VPC Route Table
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
 
 output "route_table" {
   value = aws_route_table.this
--- route_table/variables.tf
+++ route_table/variables.tf
@@ -1,32 +1,33 @@
-# --------------------------------------------------------------------------------
-# Amazon VPC Route Table variable
-# @see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
-# --------------------------------------------------------------------------------
+# Amazon VPC Route Table
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
 
 variable "route" {
-  type    = any
-  default = []
+  description = "A list of route objects"
+  type        = any
+  default     = []
 }
 
-variable "nat_gateway_ids" {
-  type    = any
-  default = []
-}
-
-variable "subnet" {
-  type = string
-}
-
-variable "subnet_ids" {
-  type    = list(string)
-  default = []
+variable "route_table_name" {
+  description = "Route table name"
+  type        = string
 }
 
 variable "tags" {
-  type    = map(string)
-  default = {}
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
 }
 
 variable "vpc_id" {
-  type = string
+  description = "VPC ID"
+  type        = string
+}
+
+# Amazon VPC Route Table Association
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table_association
+
+variable "subnet_ids" {
+  description = "A list of subnet IDs to associate with the route table"
+  type        = list(string)
+  default     = []
 }
