896999213ac5362b1e8cbf5583a95d52e447d388
maishio <masanori.ishio119@gmail.com>
2023-04-07 09:40:07 +0900

    feat: add acm resource

--- acm/main.tf
+++ acm/main.tf
@@ -1,28 +1,23 @@
-# --------------------------------------------------------------------------------
-# AWS ACM Certificate resource
-# @see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate
-# --------------------------------------------------------------------------------
+# AWS ACM Certificate
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate
 
 resource "aws_acm_certificate" "this" {
   domain_name               = var.domain_name
-  subject_alternative_names = ["*.${var.domain_name}"]
+  subject_alternative_names = var.subject_alternative_names
   validation_method         = var.validation_method
+  tags                      = merge(var.tags, { "Name" = var.domain_name })
 }
 
-# --------------------------------------------------------------------------------
-# AWS ACM Certificate Validation resource
-# @see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate_validation
-# --------------------------------------------------------------------------------
+# AWS ACM Certificate Validation
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate_validation
 
 resource "aws_acm_certificate_validation" "this" {
   certificate_arn         = aws_acm_certificate.this.arn
   validation_record_fqdns = [for record in aws_route53_record.this : record.fqdn]
 }
 
-# --------------------------------------------------------------------------------
-# Amazon Route53 Record resource
-# @see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record
-# --------------------------------------------------------------------------------
+# Amazon Route53 Record
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record
 
 resource "aws_route53_record" "this" {
   for_each = {
--- acm/outputs.tf
+++ acm/outputs.tf
@@ -1,16 +1,12 @@
-# --------------------------------------------------------------------------------
-# AWS ACM Certificate output
-# @see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate
-# --------------------------------------------------------------------------------
+# AWS ACM Certificate
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate
 
 output "acm_certificate" {
   value = aws_acm_certificate.this
 }
 
-# --------------------------------------------------------------------------------
-# AWS ACM Certificate Validation output
-# @see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate_validation
-# --------------------------------------------------------------------------------
+# AWS ACM Certificate Validation
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate_validation
 
 output "acm_certificate_validation" {
   value = aws_acm_certificate_validation.this
