ff3d3025911426474ce6f80c13a74e2c1c924bd8
maishio <masanori.ishio119@gmail.com>
2023-05-03 08:11:46 +0900

    feat: add eventbridge target resource

--- terraform/eventbridge/target/main.tf
+++ terraform/eventbridge/target/main.tf
@@ -1,23 +1,3 @@
-# Amazon EventBridge Target
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_event_target
-
-resource "aws_cloudwatch_event_rule" "this" {
-  description         = var.description
-  event_bus_name      = var.event_bus_name
-  event_pattern       = var.event_path != null ? templatefile(var.event_path, var.event_vars) : null
-  is_enabled          = var.is_enabled
-  name                = var.name
-  schedule_expression = var.schedule_expression
-  tags                = merge(var.tags, { "Name" = var.name })
-
-  lifecycle {
-    ignore_changes = [
-      is_enabled,
-      schedule_expression
-    ]
-  }
-}
-
 # Amazon Event Bridge Event Target
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_event_target
 
